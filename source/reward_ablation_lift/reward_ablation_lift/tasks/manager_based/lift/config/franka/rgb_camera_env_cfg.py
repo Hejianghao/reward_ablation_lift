90f385a8bf835a32f1c21f5eba6a1c13b7c2c895
Hejianghao <jianghaocool2006@163.com>
2026-04-23 16:13:31 +1200

    update camera

--- source/reward_ablation_lift/reward_ablation_lift/tasks/manager_based/lift/config/franka/rgb_camera_env_cfg.py
+++ source/reward_ablation_lift/reward_ablation_lift/tasks/manager_based/lift/config/franka/rgb_camera_env_cfg.py
@@ -20,18 +20,30 @@ class CameraObjectTableSceneCfg(ObjectTableSceneCfg):
         prim_path="{ENV_REGEX_NS}/Camera",
         offset=TiledCameraCfg.OffsetCfg(
             pos=(0.5, 0.0, 2.2),
-            rot=(0.7071068, 0.0, 0.7071068, 0.0),
-            convention="world",
+            # rot=(0.7071068, 0.0, 0.7071068, 0.0),
+            # convention="world",
+            rot=(0.0, 0.0, 1.0, 0.0),
+            convention="opengl",
         ),
         data_types=["rgb"],
-        spawn=sim_utils.PinholeCameraCfg(
-            focal_length=24.0,
-            focus_distance=400.0,
-            horizontal_aperture=20.955,
-            clipping_range=(0.1, 20.0),
+        spawn=sim_utils.PinholeCameraCfg.from_intrinsic_matrix(
+            intrinsic_matrix=[
+                617.7935 / 2,
+                0.0,
+                326.6838 / 2,
+                0.0,
+                617.8885 / 2,
+                240.0364 / 2,
+                0.0,
+                0.0,
+                1.0,
+            ],
+            height=240,
+            width=320,
+            clipping_range=(0.01, 10.0),
         ),
-        width=128,
-        height=128,
+        width=320,
+        height=240,
     )
 
 
@@ -59,7 +71,7 @@ class RGBCameraObservationsCfg(ObservationsCfg):
 @configclass
 class FrankaCubeLiftRGBCameraEnvCfg(FrankaCubeLiftEnvCfg):
     # Replace scene with camera-enabled version
-    scene: CameraObjectTableSceneCfg = CameraObjectTableSceneCfg(num_envs=256, env_spacing=2.5)
+    scene: CameraObjectTableSceneCfg = CameraObjectTableSceneCfg(num_envs=512, env_spacing=2.5)
     observations: RGBCameraObservationsCfg = RGBCameraObservationsCfg()
 
     def __post_init__(self):
